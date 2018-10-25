2cb781a1a3b74030f4629e417656b847c0ad5c8b
othiagoduarte <thiago-duarte@outlook.com>
2018-10-25 14:45:54 -0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -15,32 +15,3 @@ subject.next(34);
 subject.next(34);
 
 subject.complete();
-
-var observable = Observable.create(observer =>{
-    var i = 0;
-    var interval = setInterval(()=>{
-      i++;
-      observer.next(parseInt(Math.random()*10))
-      if(i > 5){
-        clearInterval(interval);
-        observer.complete();
-      }
-    }, 1000);
-  })
-  
-  
-  var subject = new Rx.Subject();
-  
-  var subSource = observable.subscribe(subject);
-  
-  var subscription = subject.subscribe(
-      value => console.log(`1 - My Value is ${value}`),
-      err => console.log(`Error message: ${err}`),
-      () => console.log('1 - Completed!')
-  )
-  
-  var subscription2 = subject.subscribe(
-      value => console.log(`2 - My Value is ${value}`),
-      err => console.log(`Error message: ${err}`),
-      () => console.log('2 - Completed!')
-  )
